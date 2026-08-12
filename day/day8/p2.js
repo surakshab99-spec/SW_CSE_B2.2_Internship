493b4c0d8ec3fdfabe5220fae82d96492ddee0c8
surakshab99-spec <surakshab99@gmail.com>
2026-08-12 12:17:28 +0530

    let

--- day/day8/p2.js
+++ day/day8/p2.js
@@ -1,3 +1,13 @@
-//js variables
+//js variables:dynamically typed
+//var|let|const
+var x=10;
+var x=20;
+console.log(x);
+//scope behaviour
+if (true){
+    var message="hi";
+
+}
+console.log(message);
 let name='suraksha';
-console.log(name)
+console.log(name);
