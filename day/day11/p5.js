920769b4e43798705945de5ad01cf987d5e0a6c5
surakshab99-spec <surakshab99@gmail.com>
2026-08-17 12:28:47 +0530

    settimeout

---
+++ day/day11/p5.js
@@ -0,0 +1,7 @@
+console.log("start")
+setTimeout(()=>{
+    console.log("inside timeout");
+},10000);
+console.log("end")
+//settimeout is a built in function thatis used to exeute the piece of code after
+//some time.by default it is asynchronus
