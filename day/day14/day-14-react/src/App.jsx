94cf81a722ffe8e8d3a624d9c217b181af3c0692
surakshab99-spec <surakshab99@gmail.com>
2026-08-20 11:49:35 +0530

    list

---
+++ day/day14/day-14-react/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import ClickDemo from "./components/eventhandler";
+import FormDemo from './components/eventhandler2';
+import ActionButtonDemo from './components/eventhandler3';
+import SimpleList from './components/listandKeysOne';
+function App() {
+  return (
+    <>
+      <ClickDemo/>
+
+      <FormDemo/>
+
+      <ActionButtonDemo/>
+
+      <SimpleList/>
+    </>
+  )
+}
+
+export default App;
+
